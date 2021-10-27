30b25a9e427f720d940bc7c59f6461a79f26a293
Lia Gaetano <lgaetano19@gmail.com>
2021-10-26 19:04:13 -0700

    Creates separate post_books endpoint.

--- app/routes.py
+++ app/routes.py
@@ -46,11 +46,9 @@ def post_books():
     new_book = Book(title=request_body["title"],
                     description=request_body["description"])
 
-    db.session.add(new_book)
     db.session.commit()
 
-    return make_response(f"Book {new_book.title} successfully created",
-                            201)
+    return jsonify(f"Book {new_book.title} successfully created"), 201
 
 @books_bp.route("/<book_id>", methods=["GET", "PUT", "DELETE"])
 def handle_book(book_id):
